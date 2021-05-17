1e526e7a8d19af8f711db45b2d11df3e87b742d0
Msiciots <f74049020@gs.ncku.edu.tw>
2021-05-18 02:14:55 +0800

    Add get realtime price

---
+++ realtime_price.py
@@ -0,0 +1,26 @@
+import requests
+import json  
+import pandas as pd
+import time
+url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_2330.tw"
+data = {'time':[],
+        'code':[],
+        'price':[],
+        'v':[],
+        'av':[]
+        }
+while True:
+    for i in range(20):
+        res = requests.get(url).json()
+        record = res['msgArray'][0]
+        t = record['d'] + "-" + record['t']
+        data['time'].append(t)
+        data['code'].append(record['c'])
+        data['price'].append(record['pz'])
+        data['v'].append(record['s'])
+        data['av'].append(record['v'])
+        time.sleep(5)
+        if i==19:
+            df = pd.DataFrame(data=data)
+            df.to_csv("data.csv", index = False)
+            # df = df.set_index('time')
