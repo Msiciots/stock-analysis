98637238b6561a66d7c23ac36b66400d94b235fe
Msiciots <f74049020@gs.ncku.edu.tw>
2021-07-04 12:54:25 +0800

    Expand extensions
    Check for:
    - Price change +-5%
    - Volume of sale/buy order exceed 500.
    - Limit up/down.

--- realtime-monitor.py
+++ realtime-monitor.py
@@ -5,6 +5,8 @@ import requests
 import yfinance as yf
 import pandas as pd
 import talib
+from get_stock_name import get_stock_name
+
 def check_time():
     h = int(datetime.now().strftime("%H"))
     m = int(datetime.now().strftime("%M"))
@@ -12,27 +14,54 @@ def check_time():
         return True
     else:
         return False
-def get_realtime_price(sid):
+def get_realtime_info(sid):
     url = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_"+sid+".tw"
-    res = requests.get(url).json()
+    try:
+        res = requests.get(url, headers={'content-type':'application/json'})
+        # res = requests.get(url).json()
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        print("Get exception!!")
+        return "-"
+    res = res.json() 
     record = res['msgArray'][0]
-    return record['z']
-def check_entry(sid,df):
-    if get_realtime_price(sid) == "-":
-        return False
-    price = float(get_realtime_price(sid))
-    if price > df['ma'][-1]:
-        return price
-    else:
-        return False
-def check_exit(sid,df):
-    if get_realtime_price(sid) == "-":
-        return False
-    price = float(get_realtime_price(sid))
-    if price < df['ma'][-1]:
-        return price
-    else:
-        return False
+    # print(record)
+    return record
+def check(sid,df,signal):
+    stock_info = get_realtime_info(sid)
+    if stock_info == '-':
+        return False  
+    # check price 
+    price = stock_info['z']
+    up_limit = stock_info['u']
+    down_limit = stock_info['w']
+    if price != '-':
+        price = float(price)
+        print(str(datetime.now())+" "+str(price))
+        if price < df['Close'][-1] * 1.05 and signal[0][0] == 0:           
+            signal[0][0]=1
+            print(signal[1][0])
+        elif price > df['Close'][-1] * 0.95 and signal[0][1] == 0:
+            signal[0][1]=1
+            print(signal[1][1])
+        if price >= float(up_limit) and signal[0][4] == 0:
+            signal[0][4]=1
+            print(signal[1][4])
+        elif price <= float(down_limit) and signal[0][5] == 0:
+            signal[0][5]=1
+            print(signal[1][5])
+    # check order volume
+    five_self_vol = stock_info['f']
+    five_self_vol = five_self_vol.split("_")
+    five_buy_vol = stock_info['g']
+    five_buy_vol = five_buy_vol.split("_")
+    for i in range(5):
+        if int(five_self_vol[i]) >= 500 and signal[0][2] == 0:
+            signal[0][2]=1
+            print(signal[1][2])
+    for i in range(5):
+        if int(five_buy_vol[i]) >= 500 and signal[0][3] == 0:
+            signal[0][3]=1
+            print(signal[1][3])
 def data_preprocess():
     today = date.today().strftime("%Y-%m-%d")
     start = date.today() - timedelta(days=30)
@@ -43,24 +72,27 @@ def data_preprocess():
     return df
 if __name__ == "__main__":
     sid=sys.argv[1]
+    stock_info = get_realtime_info(sid)
+    stock_name = stock_info['nf']
+    print("Start to monitor "+sid+":"+stock_name+" in realtime.")
+    print("--------------------------------------------------------------")
+    print("- Price change +-5%")
+    print("- Volume of sale/buy order exceed 500.")
+    print("- Limit up/down.")
+    print("--------------------------------------------------------------")
+    signal = [[0]*6,["[SIGNAL] Current price is UP to +5% of yesterday's closing price.",
+    "[SIGNAL] Current price is DOWN to -5% of yesterday's closing price.",
+    "[SIGNAL] Order of buy exceed 500 at once.",
+    "[SIGNAL] Order of self exceed 500 at once.",
+    "[SIGNAL] Today's price is limit up!",
+    "[SIGNAL] Today's price is limit down!"
+    ]]
 
-    if not check_time():
-        sys.exit("Non-trading time")  
-
-    isInMarket = False 
     df = data_preprocess()
+    # print(df)
     while True:
-        if isInMarket:
-            price = check_exit(sid,df)
-            if price:
-                isInMarket = False
-                print("[Signal] Exit, Current price is lower than MA")
-                print("Price:"+price+" MA:"+df['MA'][-1])
-        else:
-            price = check_entry(sid,df)
-            if price:
-                isInMarket = True
-                print("[Signal] Entry, Current price is higher than MA.")
-                print("Price:"+price+" MA:"+df['MA'][-1])
+        if not check_time():
+            sys.exit("Non-trading time")  
+        check(sid,df,signal)
         time.sleep(5)
         
